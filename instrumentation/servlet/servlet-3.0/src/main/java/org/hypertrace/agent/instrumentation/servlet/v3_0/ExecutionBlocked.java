87f2f6b559375b5293962896023bb23f9283da85
Pavol Loffay <p.loffay@gmail.com>
2020-10-12 11:08:20 +0200

    Scope instrumentations under instrumentation package (#16)
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- instrumentation/servlet/servlet-3.0/src/main/java/org/hypertrace/agent/instrumentation/servlet/v3_0/ExecutionBlocked.java
+++ instrumentation/servlet/servlet-3.0/src/main/java/org/hypertrace/agent/instrumentation/servlet/v3_0/ExecutionBlocked.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.hypertrace.agent.servlet.v3_0;
+package org.hypertrace.agent.instrumentation.servlet.v3_0;
 
 import net.bytebuddy.asm.Advice.OnMethodEnter;
 
