719658a6800af6fc88f7c1950424da6f149a2641
Pavol Loffay <p.loffay@gmail.com>
2020-10-19 12:09:38 +0200

    Rename to servlet 2.3 and 3.1
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- instrumentation/build.gradle.kts
+++ instrumentation/build.gradle.kts
@@ -16,8 +16,8 @@ subprojects {
 }
 
 dependencies{
-    implementation(project(":instrumentation:servlet:servlet-3.0"))
-    implementation(project(":instrumentation:servlet:servlet-2.2"))
+    implementation(project(":instrumentation:servlet:servlet-3.1"))
+    implementation(project(":instrumentation:servlet:servlet-2.3"))
 }
 
 tasks {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,10 +2,10 @@ rootProject.name = "hypertrace-otel-agent"
 
 include(":javaagent")
 include("instrumentation")
-include("instrumentation:servlet:servlet-3.0")
-findProject(":instrumentation:servlet:servlet-3.0")?.name = "servlet-3.0"
-include("instrumentation:servlet:servlet-2.2")
-findProject(":instrumentation:servlet:servlet-2.2")?.name = "servlet-2.2"
+include("instrumentation:servlet:servlet-3.1")
+findProject(":instrumentation:servlet:servlet-3.1")?.name = "servlet-3.1"
+include("instrumentation:servlet:servlet-2.3")
+findProject(":instrumentation:servlet:servlet-2.3")?.name = "servlet-2.3"
 include("smoke-tests")
 include("blocking")
 include("javaagent-tooling")
