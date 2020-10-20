e084c11c6fd26674902727ba183e4b5f02f2aa35
Pavol Loffay <p.loffay@gmail.com>
2020-10-20 15:35:57 +0200

    Add url
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- javaagent/build.gradle.kts
+++ javaagent/build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
     // pin released version or snapshot with pinned version
     // update the dependencies also in the instrumentations sub-projects
     // https://oss.jfrog.org/artifactory/oss-snapshot-local/io/opentelemetry/instrumentation/auto/
-    //
+    // https://dl.bintray.com/open-telemetry/maven/
     implementation("io.opentelemetry.javaagent", "opentelemetry-javaagent", version = "0.9.0", classifier = "all")
 }
 
