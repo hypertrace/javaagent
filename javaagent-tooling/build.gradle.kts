06431382382fcf3894505b2b150407022606c71d
Pavol Loffay <p.loffay@gmail.com>
2020-10-23 08:55:26 +0200

    Move instrumentationMuzzle configurations to javaagent-tooling
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- javaagent-tooling/build.gradle.kts
+++ javaagent-tooling/build.gradle.kts
@@ -4,8 +4,17 @@ plugins {
     `java-library`
 }
 
-dependencies{
-    implementation("io.opentelemetry.javaagent:opentelemetry-javaagent-tooling:0.9.0")
-    implementation("io.opentelemetry.javaagent:opentelemetry-javaagent-api:0.9.0")
+val instrumentationMuzzle by configurations.creating {
+    extendsFrom(configurations.implementation.get())
 }
 
+dependencies {
+    implementation("io.opentelemetry.javaagent:opentelemetry-javaagent-tooling:0.9.0")
+    implementation("io.opentelemetry.javaagent:opentelemetry-javaagent-api:0.9.0")
+    instrumentationMuzzle("io.opentelemetry.javaagent:opentelemetry-javaagent-bootstrap:0.9.0")
+    instrumentationMuzzle("net.bytebuddy:byte-buddy:1.10.10")
+    instrumentationMuzzle("net.bytebuddy:byte-buddy-agent:1.10.10")
+    instrumentationMuzzle("com.blogspot.mydailyjava:weak-lock-free:0.15")
+    instrumentationMuzzle("com.google.auto.service:auto-service:1.0-rc7")
+    instrumentationMuzzle("org.slf4j:slf4j-api:1.7.30")
+}
