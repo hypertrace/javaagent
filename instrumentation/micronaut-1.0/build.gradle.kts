e6690c1d13c14c04513f80a6b180d6498ca35f2e
Pavol Loffay <p.loffay@gmail.com>
2021-01-12 15:59:25 +0100

    Use old version
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- instrumentation/micronaut-1.0/build.gradle.kts
+++ instrumentation/micronaut-1.0/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 val versions: Map<String, String> by extra
 
-//version += mapOf<String, String>("micronaut_version" to "1.0.0")
+val micronautVersion = "1.0.0"
 
 dependencies {
     testImplementation(project(":testing-common"))
@@ -12,13 +12,10 @@ dependencies {
 
     testImplementation("io.opentelemetry.javaagent.instrumentation:opentelemetry-javaagent-netty-4.1:${versions["opentelemetry_java_agent"]}")
 
-    testImplementation("io.micronaut.test:micronaut-test-junit5:1.1.0")
-    testImplementation("io.micronaut:micronaut-http-server-netty:1.1.0")
-    testImplementation("io.micronaut:micronaut-http-client:1.1.0")
-    testImplementation("io.micronaut:micronaut-runtime:1.1.0")
-//    testImplementation("io.micronaut:micronaut-validation:1.1.0")
-    testImplementation("io.micronaut:micronaut-inject:1.1.0")
+    testImplementation("io.micronaut.test:micronaut-test-junit5:${micronautVersion}")
+    testImplementation("io.micronaut:micronaut-http-server-netty:${micronautVersion}")
+    testImplementation("io.micronaut:micronaut-runtime:${micronautVersion}")
+    testImplementation("io.micronaut:micronaut-inject:${micronautVersion}")
 
-    testAnnotationProcessor("io.micronaut:micronaut-inject-java:1.1.0")
-//    testAnnotationProcessor("io.micronaut:micronaut-validation:1.1.0")
+    testAnnotationProcessor("io.micronaut:micronaut-inject-java:${micronautVersion}")
 }
