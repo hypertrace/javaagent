71fba10fc58a582ff294853b2d8faad1915bb1c3
Pavol Loffay <p.loffay@gmail.com>
2021-01-13 17:09:04 +0100

    Test micronaut 2.x (#232)
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- instrumentation/micronaut-1.0/build.gradle.kts
+++ instrumentation/micronaut-1.0/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 val versions: Map<String, String> by extra
 
 val micronautVersion = "1.0.0"
-val micronaut2Version = "2.2.3"
 val micronautTestVersion = "1.0.0"
 
 dependencies {
@@ -21,10 +20,9 @@ dependencies {
     testAnnotationProcessor("io.micronaut:micronaut-inject-java:${micronautVersion}")
 }
 
-/**
- * Todo this does not run tests
- */
-for (version in listOf(micronautVersion, micronaut2Version)) {
+val micronaut2Version = "2.2.3"
+
+for (version in listOf(micronaut2Version)) {
     val versionedConfiguration = configurations.create("test_${version}") {
         extendsFrom(configurations.testRuntimeClasspath.get())
     }
@@ -32,17 +30,22 @@ for (version in listOf(micronautVersion, micronaut2Version)) {
         versionedConfiguration(project(":instrumentation:netty:netty-4.1"))
         versionedConfiguration("io.opentelemetry.javaagent.instrumentation:opentelemetry-javaagent-netty-4.1:${versions["opentelemetry_java_agent"]}")
         versionedConfiguration(project(":testing-common"))
-        versionedConfiguration("io.micronaut:micronaut-inject-java:${version}")
         versionedConfiguration("io.micronaut.test:micronaut-test-junit5:${micronautTestVersion}")
+        versionedConfiguration("io.micronaut:micronaut-inject-java:${version}")
         versionedConfiguration("io.micronaut:micronaut-http-server-netty:${version}")
+        versionedConfiguration("io.micronaut:micronaut-http-client:${version}")
         versionedConfiguration("io.micronaut:micronaut-runtime:${version}")
         versionedConfiguration("io.micronaut:micronaut-inject:${version}")
+        versionedConfiguration("org.junit.jupiter:junit-jupiter-api:5.7.0")
+        versionedConfiguration("org.junit.jupiter:junit-jupiter-engine:5.7.0")
     }
     val versionedTest = task<Test>("test_${version}") {
         group = "verification"
-        testClassesDirs = sourceSets["test"].output.classesDirs
-        classpath = versionedConfiguration
+        val testOuput = sourceSets.test.get().output
+        testClassesDirs = testOuput.classesDirs
+        classpath = versionedConfiguration + testOuput
+        useJUnitPlatform()
         shouldRunAfter("test")
     }
-    tasks.check { dependsOn(versionedTest) }
+    tasks.test { dependsOn(versionedTest) }
 }
