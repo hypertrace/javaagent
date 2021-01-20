df3a8f427a5039655c8c19fc7a54f5e2999f9de2
Pavol Loffay <p.loffay@gmail.com>
2021-01-20 08:08:16 +0100

    Ignore appdynamic classloaders
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- otel-extensions/src/main/java/org/hypertrace/agent/otel/extensions/HypertraceGlobalIgnoreMatcher.java
+++ otel-extensions/src/main/java/org/hypertrace/agent/otel/extensions/HypertraceGlobalIgnoreMatcher.java
@@ -38,6 +38,10 @@ public class HypertraceGlobalIgnoreMatcher implements IgnoreMatcherProvider {
 
   @Override
   public Result classloader(ClassLoader classLoader) {
+    String name = classLoader.getClass().getName();
+    if (name.startsWith("com.singularity.*")) {
+      return Result.IGNORE;
+    }
     return Result.DEFAULT;
   }
 }
