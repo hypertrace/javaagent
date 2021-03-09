cbea37a78e592b884f3c2a3e73c367fefcf904f2
Pavol Loffay <p.loffay@gmail.com>
2021-03-09 15:31:59 +0100

    Fix
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- smoke-tests/build.gradle.kts
+++ smoke-tests/build.gradle.kts
@@ -15,13 +15,14 @@ dependencies{
     testImplementation("org.testcontainers:testcontainers:1.15.2")
     testImplementation("com.squareup.okhttp3:okhttp:4.9.0")
     testImplementation("org.awaitility:awaitility:4.0.3")
-    testImplementation("io.opentelemetry:opentelemetry-proto:${versions["opentelemetry"]}")
+    testImplementation("io.opentelemetry:opentelemetry-proto:${versions["opentelemetry"]}-alpha")
     testImplementation("io.opentelemetry:opentelemetry-sdk:${versions["opentelemetry"]}")
     testImplementation("com.google.protobuf:protobuf-java-util:3.13.0")
     testImplementation("org.spockframework:spock-core:1.3-groovy-2.5")
     testImplementation("info.solidsoft.spock:spock-global-unroll:0.5.1")
     testImplementation("com.fasterxml.jackson.core:jackson-databind:2.11.2")
     testImplementation("org.codehaus.groovy:groovy-all:2.5.11")
+    testImplementation("io.opentelemetry:opentelemetry-semconv:${versions["opentelemetry"]}-alpha")
 }
 
 tasks.test {
