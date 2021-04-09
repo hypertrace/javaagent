06cc1b8713c4bde7a2d25b61895760045404b4af
Pavol Loffay <p.loffay@gmail.com>
2021-04-09 16:36:38 +0200

    Bump OTEL to 1.0.1 (#302)
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,9 +31,9 @@ subprojects {
     description = "Hypertrace OpenTelemetry Javaagent"
 
     extra.set("versions", mapOf(
-            "opentelemetry" to "1.0.0",
-            "opentelemetry_java_agent" to "1.0.0-alpha",
-            "opentelemetry_java_agent_all" to "1.0.0",
+            "opentelemetry" to "1.0.1",
+            "opentelemetry_java_agent" to "1.0.1-alpha",
+            "opentelemetry_java_agent_all" to "1.0.1",
             "byte_buddy" to "1.10.18",
                 "slf4j" to "1.7.30"
     ))
