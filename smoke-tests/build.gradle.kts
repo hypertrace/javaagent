daa927570f6e0ea295b8a5e25055d5f176ff461a
Ryan Dens <rdens@traceable.ai>
2021-04-27 11:09:01 -0400

    :arrow_up: complete uprade of protobuf

--- smoke-tests/build.gradle.kts
+++ smoke-tests/build.gradle.kts
@@ -17,7 +17,7 @@ dependencies{
     testImplementation("org.awaitility:awaitility:4.0.3")
     testImplementation("io.opentelemetry:opentelemetry-proto:${versions["opentelemetry"]}-alpha")
     testImplementation("io.opentelemetry:opentelemetry-sdk:${versions["opentelemetry"]}")
-    testImplementation("com.google.protobuf:protobuf-java-util:3.13.0")
+    testImplementation("com.google.protobuf:protobuf-java-util:3.15.8")
     testImplementation("org.spockframework:spock-core:1.3-groovy-2.5")
     testImplementation("info.solidsoft.spock:spock-global-unroll:0.5.1")
     testImplementation("com.fasterxml.jackson.core:jackson-databind:2.11.2")
