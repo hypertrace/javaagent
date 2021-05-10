cbee12ff786fceab031aeab9d18b078c6b6bc4c2
Pavol Loffay <p.loffay@gmail.com>
2021-05-10 10:45:39 +0200

    Rename servlet module (#316)
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- instrumentation/build.gradle.kts
+++ instrumentation/build.gradle.kts
@@ -30,7 +30,7 @@ subprojects {
 
 dependencies{
     implementation(project(":instrumentation:servlet:servlet-rw"))
-    implementation(project(":instrumentation:servlet:servlet-3.0-no-wrapping"))
+    implementation(project(":instrumentation:servlet:servlet-3.0"))
     implementation(project(":instrumentation:spark-2.3"))
     implementation(project(":instrumentation:grpc-1.5"))
     implementation(project(":instrumentation:okhttp:okhttp-3.0"))
