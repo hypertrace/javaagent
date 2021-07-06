f91480481c58654bbd6dfb90fa4abb212e425736
Pavol Loffay <p.loffay@gmail.com>
2021-07-06 22:25:42 +0200

    Use hypertrace SB smoke test image and test payload trucation
    Signed-off-by: Pavol Loffay <p.loffay@gmail.com>

--- smoke-tests/src/test/java/org/hypertrace/agent/smoketest/SpringBootSmokeTest.java
+++ smoke-tests/src/test/java/org/hypertrace/agent/smoketest/SpringBootSmokeTest.java
@@ -20,28 +20,34 @@ import io.opentelemetry.proto.collector.trace.v1.ExportTraceServiceRequest;
 import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
+import java.util.stream.Collectors;
+import okhttp3.MediaType;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
+import org.hypertrace.agent.core.instrumentation.HypertraceSemanticAttributes;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.GenericContainer;
 
-// @org.junitpioneer.jupiter.SetEnvironmentVariable(
+//@org.junitpioneer.jupiter.SetEnvironmentVariable(
 //    key = "SMOKETEST_JAVAAGENT_PATH",
 //    value =
-//
-// "/Users/ploffay/projects/hypertrace/javaagent/javaagent/build/libs/hypertrace-agent-1.0.1-SNAPSHOT-all.jar")
+//        "/home/ploffay/projects/hypertrace/javaagent/javaagent/build/libs/hypertrace-agent-1.1.1-SNAPSHOT-all.jar")
 public class SpringBootSmokeTest extends AbstractSmokeTest {
+
+  private static final int DEFAULT_MAX_PAYLOAD_CAPTURE_SIZE = 128 * 1024;
 
   @Override
   protected String getTargetImage(int jdk) {
-    return "ghcr.io/open-telemetry/java-test-containers:smoke-springboot-jdk"
+    return "hypertrace/java-agent-test-containers:smoke-springboot-jdk"
         + jdk
-        + "-20210218.577304949";
+        + "-20210706.1005057969";
   }
 
   private static GenericContainer app;
@@ -63,12 +69,18 @@ public class SpringBootSmokeTest extends AbstractSmokeTest {
   }
 
   @Test
-  public void get() throws IOException, InterruptedException {
-    String url = String.format("http://localhost:%d/greeting", app.getMappedPort(8080));
-    Request request = new Request.Builder().url(url).get().build();
+  public void postJson() throws IOException, InterruptedException {
+    String url = String.format("http://localhost:%d/echo", app.getMappedPort(8080));
+    String requestBody = "request_body";
+    Request request =
+        new Request.Builder()
+            .url(url)
+            .addHeader("Content-Type", "application/json")
+            .post(RequestBody.create(requestBody, MediaType.parse("application/json")))
+            .build();
 
     try (Response response = client.newCall(request).execute()) {
-      Assertions.assertEquals(response.body().string(), "Hi!");
+      Assertions.assertEquals(response.body().string(), requestBody);
     }
     ArrayList<ExportTraceServiceRequest> traces = new ArrayList<>(waitForTraces());
 
@@ -107,8 +119,8 @@ public class SpringBootSmokeTest extends AbstractSmokeTest {
             .getValue()
             .getStringValue());
 
-    Assertions.assertEquals(1, countSpansByName(traces, "/greeting"));
-    Assertions.assertEquals(1, countSpansByName(traces, "WebController.greeting"));
+    Assertions.assertEquals(1, countSpansByName(traces, "/post"));
+    Assertions.assertEquals(1, countSpansByName(traces, "WebController.post"));
     Assertions.assertTrue(
         getInstrumentationLibSpanStream(traces)
             .anyMatch(
@@ -132,19 +144,81 @@ public class SpringBootSmokeTest extends AbstractSmokeTest {
                 .count()
             > 0);
 
-    // OTEL BS smoke test app does not have an endpoint that uses content type what we capture
-    // enable once we add smoke tests apps to our build.
-    //    List<String> responseBodyAttributes =
-    //        getSpanStream(traces)
-    //            .flatMap(span -> span.getAttributesList().stream())
-    //            .filter(
-    //                attribute ->
-    //                    attribute
-    //                        .getKey()
-    //                        .contains(HypertraceSemanticAttributes.HTTP_RESPONSE_BODY.getKey()))
-    //            .map(attribute -> attribute.getValue().getStringValue())
-    //            .collect(Collectors.toList());
-    //    Assertions.assertEquals(1, responseBodyAttributes.size());
-    //    Assertions.assertEquals("Hi!", responseBodyAttributes.get(0));
+    List<String> responseBodyAttributes =
+        getSpanStream(traces)
+            .flatMap(span -> span.getAttributesList().stream())
+            .filter(
+                attribute ->
+                    attribute
+                        .getKey()
+                        .contains(HypertraceSemanticAttributes.HTTP_RESPONSE_BODY.getKey()))
+            .map(attribute -> attribute.getValue().getStringValue())
+            .collect(Collectors.toList());
+    Assertions.assertEquals(1, responseBodyAttributes.size());
+    Assertions.assertEquals(requestBody, responseBodyAttributes.get(0));
+    List<String> requestBodyAttributes =
+        getSpanStream(traces)
+            .flatMap(span -> span.getAttributesList().stream())
+            .filter(
+                attribute ->
+                    attribute
+                        .getKey()
+                        .contains(HypertraceSemanticAttributes.HTTP_REQUEST_BODY.getKey()))
+            .map(attribute -> attribute.getValue().getStringValue())
+            .collect(Collectors.toList());
+    Assertions.assertEquals(1, requestBodyAttributes.size());
+    Assertions.assertEquals(requestBody, requestBodyAttributes.get(0));
+  }
+
+  @Test
+  public void postJson_payload_truncation() throws IOException {
+    String url = String.format("http://localhost:%d/echo", app.getMappedPort(8080));
+    String requestBody = createRequestBody(DEFAULT_MAX_PAYLOAD_CAPTURE_SIZE + 150, 'a');
+    Request request =
+        new Request.Builder()
+            .url(url)
+            .addHeader("Content-Type", "application/json")
+            .post(RequestBody.create(requestBody, MediaType.parse("application/json")))
+            .build();
+
+    try (Response response = client.newCall(request).execute()) {
+      Assertions.assertEquals(response.body().string(), requestBody);
+    }
+    ArrayList<ExportTraceServiceRequest> traces = new ArrayList<>(waitForTraces());
+
+    List<String> responseBodyAttributes =
+        getSpanStream(traces)
+            .flatMap(span -> span.getAttributesList().stream())
+            .filter(
+                attribute ->
+                    attribute
+                        .getKey()
+                        .contains(HypertraceSemanticAttributes.HTTP_RESPONSE_BODY.getKey()))
+            .map(attribute -> attribute.getValue().getStringValue())
+            .collect(Collectors.toList());
+    Assertions.assertEquals(1, responseBodyAttributes.size());
+    Assertions.assertEquals(
+        DEFAULT_MAX_PAYLOAD_CAPTURE_SIZE, responseBodyAttributes.get(0).length());
+    List<String> requestBodyAttributes =
+        getSpanStream(traces)
+            .flatMap(span -> span.getAttributesList().stream())
+            .filter(
+                attribute ->
+                    attribute
+                        .getKey()
+                        .contains(HypertraceSemanticAttributes.HTTP_REQUEST_BODY.getKey()))
+            .map(attribute -> attribute.getValue().getStringValue())
+            .collect(Collectors.toList());
+    Assertions.assertEquals(1, requestBodyAttributes.size());
+    Assertions.assertEquals(
+        DEFAULT_MAX_PAYLOAD_CAPTURE_SIZE, requestBodyAttributes.get(0).length());
+  }
+
+  private String createRequestBody(int size, char item) {
+    StringBuilder stringBuilder = new StringBuilder();
+    for (int i = 0; i < size; i++) {
+      stringBuilder.append(item);
+    }
+    return stringBuilder.toString();
   }
 }
