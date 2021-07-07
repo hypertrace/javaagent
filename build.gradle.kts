c8f8e3b1f8022b369180679e6ae3e7e22f9a9818
Ryan Dens <rdens@traceable.ai>
2021-07-07 09:36:36 -0400

    :arrow_up: upgrade to 1.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,8 +33,8 @@ subprojects {
 
     extra.set("versions", mapOf(
             "opentelemetry" to "1.2.0",
-            "opentelemetry_java_agent" to "1.2.0-alpha",
-            "opentelemetry_java_agent_all" to "1.2.0",
+            "opentelemetry_java_agent" to "1.3.1-alpha",
+            "opentelemetry_java_agent_all" to "1.3.1",
             "byte_buddy" to "1.10.18",
                 "slf4j" to "1.7.30"
     ))
