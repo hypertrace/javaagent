31a8e8437867fcba4a9827e7a5f5788ff6eb5125
Ryan Dens <rdens@traceable.ai>
2021-07-20 20:59:38 -0400

    :bug: copy-paste build script bug

--- instrumentation/grpc-shaded-netty-1.9/build.gradle.kts
+++ instrumentation/grpc-shaded-netty-1.9/build.gradle.kts
@@ -14,6 +14,14 @@ muzzle {
     }
 }
 
+afterEvaluate{
+    io.opentelemetry.instrumentation.gradle.bytebuddy.ByteBuddyPluginConfigurator(project,
+            sourceSets.main.get(),
+            "io.opentelemetry.javaagent.tooling.muzzle.collector.MuzzleCodeGenerationPlugin",
+            project(":javaagent-tooling").configurations["instrumentationMuzzle"] + configurations.runtimeClasspath
+    ).configure()
+}
+
 dependencies {
     compileOnly("io.grpc:grpc-core:1.9.0")
     compileOnly("io.grpc:grpc-netty-shaded:1.9.0")
