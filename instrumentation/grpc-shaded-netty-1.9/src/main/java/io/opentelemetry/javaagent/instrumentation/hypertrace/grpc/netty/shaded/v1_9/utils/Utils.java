bd25277a7dc7ac3023ecc6c338f6c01babaaa2aa
Ryan Dens <rdens@traceable.ai>
2021-07-20 20:09:41 -0400

    :art: format utils

--- instrumentation/grpc-shaded-netty-1.9/src/main/java/io/opentelemetry/javaagent/instrumentation/hypertrace/grpc/netty/shaded/v1_9/utils/Utils.java
+++ instrumentation/grpc-shaded-netty-1.9/src/main/java/io/opentelemetry/javaagent/instrumentation/hypertrace/grpc/netty/shaded/v1_9/utils/Utils.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright The Hypertrace Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.opentelemetry.javaagent.instrumentation.hypertrace.grpc.netty.shaded.v1_9.utils;
 
 import io.grpc.Metadata;
@@ -24,9 +40,8 @@ public class Utils {
     }
   }
 
-  public static void handleConvertClientHeaders(Object scheme, Object defaultPath, Object authority,
-      Object method,
-      Span currentSpan) {
+  public static void handleConvertClientHeaders(
+      Object scheme, Object defaultPath, Object authority, Object method, Span currentSpan) {
     if (scheme != null) {
       currentSpan.setAttribute(
           HypertraceSemanticAttributes.rpcRequestMetadata(
