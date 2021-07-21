699943833e9ae2e4068f6cb23413a8e3b738567b
Ryan Dens <rdens@traceable.ai>
2021-07-21 13:29:28 -0400

    :fire: remove unneeded relocations

--- shaded-protobuf-java-util/build.gradle.kts
+++ shaded-protobuf-java-util/build.gradle.kts
@@ -13,11 +13,4 @@ dependencies {
 tasks.shadowJar {
     relocate("com.google.protobuf.util", "io.opentelemetry.javaagent.instrumentation.hypertrace.com.google.protobuf.util")
     relocate("com.google.gson", "io.opentelemetry.javaagent.instrumentation.hypertrace.com.google.gson")
-//    relocate("com.google.common", "io.opentelemetry.javaagent.instrumentation.hypertrace.com.google.common")
-//    relocate("com.google.errorprone", "io.opentelemetry.javaagent.instrumentation.hypertrace.com.google.errorprone")
-//    relocate("com.google.thirdparty", "io.opentelemetry.javaagent.instrumentation.hypertrace.com.google.thirdparty")
-
-
-//    relocate("com.google", "io.opentelemetry.javaagent.instrumentation.hypertrace.com.google")
-
 }
