d7d2c462e7f7343874ca1d7ccb6db7b26fc9afef
Ryan Dens <rdens@traceable.ai>
2021-07-22 22:25:02 -0400

    :recycle: move configuration of instrumentation subproject dependencies to plugin

--- instrumentation/build.gradle.kts
+++ instrumentation/build.gradle.kts
@@ -3,32 +3,6 @@ plugins {
     `java-library`
 }
 
-val versions: Map<String, String> by extra
-
-subprojects {
-    dependencies {
-        implementation("org.slf4j:slf4j-api:1.7.30")
-        compileOnly("com.google.auto.service:auto-service-annotations:1.0")
-        annotationProcessor("com.google.auto.service:auto-service:1.0")
-        implementation("net.bytebuddy:byte-buddy:${versions["byte_buddy"]}")
-
-        implementation("io.opentelemetry:opentelemetry-api:${versions["opentelemetry"]}")
-        implementation("io.opentelemetry.javaagent:opentelemetry-javaagent-tooling:${versions["opentelemetry_java_agent"]}")
-        implementation("io.opentelemetry.javaagent:opentelemetry-javaagent-extension-api:${versions["opentelemetry_java_agent"]}")
-        implementation("io.opentelemetry.javaagent:opentelemetry-javaagent-api:${versions["opentelemetry_java_agent"]}")
-        implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-api:${versions["opentelemetry_java_agent"]}")
-        implementation(project(":javaagent-core"))
-        implementation(project(":filter-api"))
-    }
-
-    // This ensures to build jars for all dependencies in instrumentation module for ByteBuddy
-    configurations.named(JavaPlugin.COMPILE_CLASSPATH_CONFIGURATION_NAME) {
-        attributes {
-            attribute(LibraryElements.LIBRARY_ELEMENTS_ATTRIBUTE, objects.named<LibraryElements>(LibraryElements.JAR))
-        }
-    }
-}
-
 dependencies{
     implementation(project(":instrumentation:servlet:servlet-rw"))
     implementation(project(":instrumentation:servlet:servlet-3.0"))
