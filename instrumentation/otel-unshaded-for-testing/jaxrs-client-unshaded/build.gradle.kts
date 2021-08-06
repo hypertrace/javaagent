66cfd308c0b2d8bde22935a819a4b429ba98eca6
Ryan Dens <rdens@traceable.ai>
2021-08-06 09:47:02 -0400

    :bug: fix compile errors with jaxrs-client

--- instrumentation/otel-unshaded-for-testing/jaxrs-client-unshaded/build.gradle.kts
+++
@@ -1,9 +0,0 @@
-plugins {
-    id("io.opentelemetry.instrumentation.un-shade")
-}
-
-val versions: Map<String, String> by extra
-
-dependencies {
-    api("io.opentelemetry.javaagent.instrumentation:opentelemetry-javaagent-jaxrs-client-2.0-common:${versions["opentelemetry_java_agent"]}:all")
-}
