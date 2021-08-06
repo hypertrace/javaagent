ea5bc96f1736cf1f0c181ce4dcfa2f4d875e0b3c
Ryan Dens <rdens@traceable.ai>
2021-08-06 09:55:53 -0400

    :bug: fix compile bugs with struts-2.3

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -70,7 +70,5 @@ include("instrumentation:undertow:undertow-servlet-1.4")
 findProject(":instrumentation:undertow:undertow-servlet-1.4")?.name = "undertow-servlet-1.4"
 include("instrumentation:vertx:otel-vertx-web-3.0-unshaded-for-instrumentation")
 findProject(":instrumentation:vertx:otel-vertx-web-3.0-unshaded-for-instrumentation")?.name = "otel-vertx-web-3.0-unshaded-for-instrumentation"
-include("instrumentation:otel-unshaded-for-testing:servlet-unshaded")
-findProject(":instrumentation:otel-unshaded-for-testing:servlet-unshaded")?.name = "servlet-unshaded"
 include("instrumentation:otel-unshaded-for-testing:okhttp-unshaded")
 findProject(":instrumentation:otel-unshaded-for-testing:okhttp-unshaded")?.name = "okhttp-unshaded"
