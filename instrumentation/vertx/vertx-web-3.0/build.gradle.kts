90a66a1d98ded8ed945d7dbe0edc97c6b24ee97d
Ryan Dens <rdens@traceable.ai>
2021-08-30 09:15:38 -0400

    :fire: remove muzzle comment

--- instrumentation/vertx/vertx-web-3.0/build.gradle.kts
+++ instrumentation/vertx/vertx-web-3.0/build.gradle.kts
@@ -18,7 +18,6 @@ muzzle {
         group = "io.vertx"
         module = "vertx-web"
         versions = "[3.0.0,4.0.0)"
-//        extraDependency("io.netty:netty-codec-http:4.0.38.Final")
     }
 }
 
