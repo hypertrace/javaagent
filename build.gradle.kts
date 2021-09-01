8f0ac24eab6352fe7bc9ea1cd92bf741ab4d421e
Ryan Dens <rdens@traceable.ai>
2021-08-31 21:37:35 -0400

    :arrow_up: upgrade otel instrumentation version

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,8 +33,8 @@ subprojects {
 
     extra.set("versions", mapOf(
             "opentelemetry" to "1.5.0",
-            "opentelemetry_java_agent" to "1.5.2-alpha",
-            "opentelemetry_java_agent_all" to "1.5.2",
+            "opentelemetry_java_agent" to "1.5.3-alpha",
+            "opentelemetry_java_agent_all" to "1.5.3",
             "byte_buddy" to "1.11.2",
                 "slf4j" to "1.7.30"
     ))
