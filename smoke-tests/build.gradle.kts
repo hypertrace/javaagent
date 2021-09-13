201b28ac32ee77a349e9ec898aa65dd3bcf54c13
Ryan Dens <rdens@traceable.ai>
2021-09-13 09:02:33 -0400

    :heavy_plus_sign: add grpc to smoke tests

--- smoke-tests/build.gradle.kts
+++ smoke-tests/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies{
     testImplementation("com.squareup.okhttp3:okhttp:4.9.0")
     testImplementation("org.awaitility:awaitility:4.0.3")
     testImplementation("io.opentelemetry:opentelemetry-proto:${versions["opentelemetry"]}-alpha")
+    testImplementation("io.grpc:grpc-core:1.36.1") // needed at runtime to send gRPC requests to the gRPC app
     testRuntimeOnly("io.grpc:grpc-netty-shaded:1.36.1") // needed at runtime to send gRPC requests to the gRPC app
     testImplementation("io.opentelemetry:opentelemetry-sdk:${versions["opentelemetry"]}")
     testImplementation("com.google.protobuf:protobuf-java-util:3.15.8")
