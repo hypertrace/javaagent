2200474c0e8576d5d10b18727fbede23fea1506f
Ryan Dens <rdens@traceable.ai>
2021-11-05 11:06:39 -0400

    :sparkles: rovide headers to ByteBufferSpanPair from SpanAndObject pair

--- instrumentation/servlet/servlet-3.0/src/main/java/io/opentelemetry/javaagent/instrumentation/hypertrace/servlet/v3_0/nowrapping/request/ServletRequestInstrumentation.java
+++ instrumentation/servlet/servlet-3.0/src/main/java/io/opentelemetry/javaagent/instrumentation/hypertrace/servlet/v3_0/nowrapping/request/ServletRequestInstrumentation.java
@@ -27,7 +27,6 @@ import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
 import io.opentelemetry.javaagent.instrumentation.api.ContextStore;
 import io.opentelemetry.javaagent.instrumentation.api.InstrumentationContext;
 import java.io.BufferedReader;
-import java.util.Collections;
 import javax.servlet.ServletInputStream;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
@@ -110,7 +109,7 @@ public class ServletRequestInstrumentation implements TypeInstrumentation {
 
       ByteBufferSpanPair bufferSpanPair =
           Utils.createRequestByteBufferSpanPair(
-              httpServletRequest, spanAndObjectPair.getSpan(), Collections.emptyMap());
+              httpServletRequest, spanAndObjectPair.getSpan(), spanAndObjectPair.getHeaders());
       contextStore.put(servletInputStream, bufferSpanPair);
       spanAndObjectPair.setAssociatedObject(servletInputStream);
     }
