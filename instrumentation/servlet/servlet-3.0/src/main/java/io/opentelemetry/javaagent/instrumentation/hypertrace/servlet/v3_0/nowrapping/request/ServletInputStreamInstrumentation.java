433fecd7cb2c769de947ea05aba0a9fe3dca7ad4
Ryan Dens <rdens@traceable.ai>
2021-11-11 15:49:15 -0500

    :construction: functional custom exception handler

--- instrumentation/servlet/servlet-3.0/src/main/java/io/opentelemetry/javaagent/instrumentation/hypertrace/servlet/v3_0/nowrapping/request/ServletInputStreamInstrumentation.java
+++ instrumentation/servlet/servlet-3.0/src/main/java/io/opentelemetry/javaagent/instrumentation/hypertrace/servlet/v3_0/nowrapping/request/ServletInputStreamInstrumentation.java
@@ -26,10 +26,20 @@ import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeTransformer;
 import io.opentelemetry.javaagent.instrumentation.api.InstrumentationContext;
+import io.opentelemetry.javaagent.tooling.Utils;
 import java.io.IOException;
 import javax.servlet.ServletInputStream;
+import net.bytebuddy.ClassFileVersion;
+import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
+import net.bytebuddy.asm.Advice.ExceptionHandler;
 import net.bytebuddy.description.type.TypeDescription;
+import net.bytebuddy.implementation.Implementation;
+import net.bytebuddy.implementation.bytecode.StackManipulation;
+import net.bytebuddy.jar.asm.Label;
+import net.bytebuddy.jar.asm.MethodVisitor;
+import net.bytebuddy.jar.asm.Opcodes;
+import net.bytebuddy.jar.asm.Type;
 import net.bytebuddy.matcher.ElementMatcher;
 import org.hypertrace.agent.core.instrumentation.HypertraceCallDepthThreadLocalMap;
 import org.hypertrace.agent.core.instrumentation.HypertraceEvaluationException;
@@ -45,10 +55,16 @@ public class ServletInputStreamInstrumentation implements TypeInstrumentation {
 
   @Override
   public void transform(TypeTransformer transformer) {
-
-    transformer.applyAdviceToMethod(
-        named("read").and(takesArguments(0)).and(isPublic()),
-        ServletInputStreamInstrumentation.class.getName() + "$InputStream_ReadNoArgs");
+    transformer.applyTransformer(
+        new AgentBuilder.Transformer.ForAdvice()
+            .include(
+                io.opentelemetry.javaagent.tooling.Utils.getBootstrapProxy(),
+                io.opentelemetry.javaagent.tooling.Utils.getAgentClassLoader(),
+                Utils.getExtensionsClassLoader())
+            .withExceptionHandler(EXCEPTION_STACK_HANDLER)
+            .advice(
+                named("read").and(takesArguments(0)).and(isPublic()),
+                ServletInputStreamInstrumentation.class.getName() + "$InputStream_ReadNoArgs"));
     transformer.applyAdviceToMethod(
         named("read")
             .and(takesArguments(1))
@@ -271,4 +287,84 @@ public class ServletInputStreamInstrumentation implements TypeInstrumentation {
       }
     }
   }
+
+  private static final String HANDLER_NAME =
+      "io.opentelemetry.javaagent.bootstrap.ExceptionLogger".replace('.', '/');
+  private static final String LOGGER_NAME = "org.slf4j.Logger".replace('.', '/');
+  private static final String LOG_FACTORY_NAME = "org.slf4j.LoggerFactory".replace('.', '/');
+  private static final ExceptionHandler EXCEPTION_STACK_HANDLER =
+      new ExceptionHandler.Simple(
+          new StackManipulation() {
+            // Pops one Throwable off the stack. Maxes the stack to at least 3.
+            private final StackManipulation.Size size = new StackManipulation.Size(-1, 3);
+
+            @Override
+            public boolean isValid() {
+              return true;
+            }
+
+            @Override
+            public StackManipulation.Size apply(MethodVisitor mv, Implementation.Context context) {
+              String name = context.getInstrumentedType().getName();
+              ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+
+              // writes the following bytecode:
+              // try {
+              //   org.slf4j.LoggerFactory.getLogger((Class)ExceptionLogger.class)
+              //     .debug("exception in instrumentation", t);
+              // } catch (Throwable t2) {
+              // }
+              Label logStart = new Label();
+              Label logEnd = new Label();
+              Label eatException = new Label();
+              Label handlerExit = new Label();
+
+              // Frames are only meaningful for class files in version 6 or later.
+              boolean frames = context.getClassFileVersion().isAtLeast(ClassFileVersion.JAVA_V6);
+
+              mv.visitTryCatchBlock(logStart, logEnd, eatException, "java/lang/Throwable");
+
+              // stack: (top) throwable
+              mv.visitLabel(logStart);
+              mv.visitLdcInsn(Type.getType("L" + HANDLER_NAME + ";"));
+              mv.visitMethodInsn(
+                  Opcodes.INVOKESTATIC,
+                  LOG_FACTORY_NAME,
+                  "getLogger",
+                  "(Ljava/lang/Class;)L" + LOGGER_NAME + ";",
+                  /* isInterface= */ false);
+              mv.visitInsn(Opcodes.SWAP); // stack: (top) throwable,logger
+              mv.visitLdcInsn(
+                  "Failed to handle exception in instrumentation for "
+                      + name
+                      + " on "
+                      + classLoader);
+              mv.visitInsn(Opcodes.SWAP); // stack: (top) throwable,string,logger
+              mv.visitMethodInsn(
+                  Opcodes.INVOKEINTERFACE,
+                  LOGGER_NAME,
+                  "debug",
+                  "(Ljava/lang/String;Ljava/lang/Throwable;)V",
+                  /* isInterface= */ true);
+              mv.visitLabel(logEnd);
+              mv.visitJumpInsn(Opcodes.GOTO, handlerExit);
+
+              // if the runtime can't reach our ExceptionHandler or logger,
+              //   silently eat the exception
+              mv.visitLabel(eatException);
+              if (frames) {
+                mv.visitFrame(Opcodes.F_SAME1, 0, null, 1, new Object[] {"java/lang/Throwable"});
+              }
+              mv.visitInsn(Opcodes.POP);
+              // mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/lang/Throwable",
+              //    "printStackTrace", "()V", false);
+
+              mv.visitLabel(handlerExit);
+              if (frames) {
+                mv.visitFrame(Opcodes.F_SAME, 0, null, 0, null);
+              }
+
+              return size;
+            }
+          });
 }
