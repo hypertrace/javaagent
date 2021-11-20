ef6cb7dd011b3ffeb5de926aebbe729b4777ce15
Shashank Patidar <shashank@traceable.ai>
2021-11-20 11:22:12 +0530

    fix vulnerabilities

--- testing-bootstrap/build.gradle.kts
+++ testing-bootstrap/build.gradle.kts
@@ -16,9 +16,7 @@ dependencies {
     implementation(project(":javaagent-core"))
     implementation(project(":filter-api"))
 
-    implementation("ch.qos.logback:logback-classic:1.2.3") {
-        exclude("ch.qos.logback:logback-core@1.2.3")
-    }
+    implementation("ch.qos.logback:logback-classic:1.2.7")
     implementation("org.slf4j:slf4j-api:${versions["slf4j"]}")
 }
 
