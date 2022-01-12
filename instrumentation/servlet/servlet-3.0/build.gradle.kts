9cee6705e4a2bee13a592e9d55bdeb962aa33d47
Ryan Dens <rdens@traceable.ai>
2022-01-12 13:10:31 -0800

    :bug: add compile dependencies on relocated otel servlet objects

--- instrumentation/servlet/servlet-3.0/build.gradle.kts
+++ instrumentation/servlet/servlet-3.0/build.gradle.kts
@@ -30,7 +30,8 @@ afterEvaluate{
 val versions: Map<String, String> by extra
 
 dependencies {
-    implementation("io.opentelemetry.instrumentation:opentelemetry-servlet-3.0:${versions["opentelemetry_java_agent"]}")
+    compileOnly("io.opentelemetry.instrumentation:opentelemetry-servlet-common:${versions["opentelemetry_java_agent"]}")
+    compileOnly("io.opentelemetry.javaagent.instrumentation:opentelemetry-javaagent-servlet-3.0:${versions["opentelemetry_java_agent"]}") // Servlet3Accessor
     testImplementation("io.opentelemetry.javaagent.instrumentation:opentelemetry-javaagent-servlet-3.0:${versions["opentelemetry_java_agent"]}")
     compileOnly("io.opentelemetry.javaagent:opentelemetry-javaagent-bootstrap:${versions["opentelemetry_java_agent"]}")
     compileOnly("javax.servlet:javax.servlet-api:3.1.0")
