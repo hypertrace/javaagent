41c4e89dce3621a0fb6782390cf9dcdade0561bc
bradAtTraceable <brad@traceable.ai>
2022-05-27 14:46:55 -0700

    Switch to version 2.13.3 of jackson.dataformat

--- otel-extensions/build.gradle.kts
+++ otel-extensions/build.gradle.kts
@@ -47,7 +47,7 @@ dependencies {
     api("com.google.protobuf:protobuf-java")
     api("com.google.protobuf:protobuf-java-util")
     // convert yaml to json, since java protobuf impl supports only json
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.6")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.3")
     // fix vulnerability
     constraints {
         api("com.google.code.gson:gson:2.8.9")
