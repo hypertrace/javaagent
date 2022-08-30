d860b141f5c7e92a74d7ccd7e8eb89f1e9de0dbd
bradAtTraceable <brad@traceable.ai>
2022-08-30 15:20:22 -0700

    Circumvent SNYK-JAVA-ORGYAML-2806360

--- javaagent-core/build.gradle.kts
+++ javaagent-core/build.gradle.kts
@@ -9,5 +9,13 @@ dependencies {
     api("io.opentelemetry:opentelemetry-api:${versions["opentelemetry"]}")
     api("io.opentelemetry.javaagent:opentelemetry-javaagent-instrumentation-api:${versions["opentelemetry_java_agent"]}")
     implementation("org.slf4j:slf4j-api:${versions["slf4j"]}")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.3")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.13.3") {
+        constraints {
+            implementation("org.yaml:snakeyaml:1.31") {
+                because(
+                    "SNYK error SNYK-JAVA-ORGYAML-2806360"
+                )
+            }
+        }
+    }
 }
