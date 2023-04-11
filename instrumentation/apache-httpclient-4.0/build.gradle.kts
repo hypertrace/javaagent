b62d0ee24335c58d747ebbd9c5295dddceadc08f
Tyrone Yu <tyrone@traceable.ai>
2023-04-11 14:46:18 -0700

    Omit dropwizard client 3.0,4.0

--- instrumentation/apache-httpclient-4.0/build.gradle.kts
+++ instrumentation/apache-httpclient-4.0/build.gradle.kts
@@ -23,7 +23,7 @@ muzzle {
         // We want to support the dropwizard clients too.
         group = "io.dropwizard"
         module = "dropwizard-client"
-        versions = "(,)"
+        versions = "(,3.0)"
         assertInverse = true
     }
 }
