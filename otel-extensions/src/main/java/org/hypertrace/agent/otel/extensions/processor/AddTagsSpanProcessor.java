6e851f81aca3ea7a3c1fbf35145a35cb51bd08aa
Shashank Patidar <74622220+shashank11p@users.noreply.github.com>
2023-10-17 23:59:09 +0530

    add hostname to attributes (#390)

--- otel-extensions/src/main/java/org/hypertrace/agent/otel/extensions/processor/AddTagsSpanProcessor.java
+++ otel-extensions/src/main/java/org/hypertrace/agent/otel/extensions/processor/AddTagsSpanProcessor.java
@@ -23,22 +23,37 @@ import io.opentelemetry.sdk.trace.ReadableSpan;
 import io.opentelemetry.sdk.trace.SpanProcessor;
 import io.opentelemetry.semconv.resource.attributes.ResourceAttributes;
 import org.hypertrace.agent.otel.extensions.CgroupsReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AddTagsSpanProcessor implements SpanProcessor {
 
+  private static final Logger log = LoggerFactory.getLogger(AddTagsSpanProcessor.class.getName());
+
   // initialize at startup because the processor is executed for every span.
-  private String containerId;
+  private final String containerId;
+  private final String hostName;
 
   /** Note - the container id is not available using this technique if cgroup2 is installed. */
   public AddTagsSpanProcessor() {
     CgroupsReader cgroupsReader = new CgroupsReader();
     containerId = cgroupsReader.readContainerId();
+    String hostnameEnv = "";
+    try {
+      hostnameEnv = System.getenv("HOSTNAME");
+    } catch (SecurityException e) {
+      log.error("could not get hostname", e);
+    }
+    hostName = hostnameEnv;
   }
 
   @Override
   public void onStart(Context parentContext, ReadWriteSpan span) {
     if (containerId != null && !containerId.isEmpty()) {
       span.setAttribute(ResourceAttributes.CONTAINER_ID, containerId);
+    }
+    if (hostName != null && !hostName.isEmpty()) {
+      span.setAttribute(ResourceAttributes.HOST_NAME, hostName);
     }
   }
 
