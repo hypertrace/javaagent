669316f6095754ee117b6fdb4c7ea9afb3f7be32
kishansairam9 <kishan.adapa@traceable.ai>
2024-01-23 23:14:54 +0530

    try move constraint

--- testing-common/build.gradle.kts
+++ testing-common/build.gradle.kts
@@ -11,18 +11,9 @@ dependencies {
 
     testFixturesCompileOnly("org.junit.jupiter:junit-jupiter-api:5.7.0")
     testFixturesRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
-    testFixturesApi(platform("io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom-alpha:${versions["opentelemetry_instrumentation_bom_alpha"]}"))
-    testFixturesImplementation("org.junit-pioneer:junit-pioneer:1.0.0")
-    testFixturesApi("io.opentelemetry:opentelemetry-api")
-    testFixturesApi("io.opentelemetry:opentelemetry-sdk")
-    testFixturesCompileOnly("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure-alpha")
-    testFixturesApi("com.squareup.okhttp3:okhttp:4.9.0")
-    testFixturesApi("com.squareup.okhttp3:logging-interceptor:4.9.0")
-    testFixturesImplementation("io.opentelemetry:opentelemetry-exporter-logging")
-    testFixturesImplementation("io.opentelemetry.javaagent:opentelemetry-javaagent-bootstrap")
-    testFixturesImplementation("io.opentelemetry.javaagent:opentelemetry-javaagent-tooling") {
+    testFixturesApi(platform("io.opentelemetry.instrumentation:opentelemetry-instrumentation-bom-alpha:${versions["opentelemetry_instrumentation_bom_alpha"]}")) {
         constraints {
-            implementation("io.opentelemetry.javaagent:opentelemetry-javaagent-tooling-java9:1.7.2-alpha") {// TODO: change
+            api("io.opentelemetry.javaagent:opentelemetry-javaagent-tooling-java9") {
                 attributes {
                     // this transitive dependency creates classes compatible with Java 9 and up, but is only referenced in safe ways for
                     // java 8 by the javaagent-tooling dependency
@@ -31,6 +22,15 @@ dependencies {
             }
         }
     }
+    testFixturesImplementation("org.junit-pioneer:junit-pioneer:1.0.0")
+    testFixturesApi("io.opentelemetry:opentelemetry-api")
+    testFixturesApi("io.opentelemetry:opentelemetry-sdk")
+    testFixturesCompileOnly("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure-alpha")
+    testFixturesApi("com.squareup.okhttp3:okhttp:4.9.0")
+    testFixturesApi("com.squareup.okhttp3:logging-interceptor:4.9.0")
+    testFixturesImplementation("io.opentelemetry:opentelemetry-exporter-logging")
+    testFixturesImplementation("io.opentelemetry.javaagent:opentelemetry-javaagent-bootstrap")
+    testFixturesImplementation("io.opentelemetry.javaagent:opentelemetry-javaagent-tooling")
     testFixturesImplementation("io.opentelemetry.javaagent:opentelemetry-javaagent-extension-api")
     testFixturesImplementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-api")
     testFixturesImplementation("ch.qos.logback:logback-classic:1.4.6")
